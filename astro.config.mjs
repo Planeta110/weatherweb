87e1766435c7f34e10da61c0abc3e4a68695d252
Planeta110 <143905008+Planeta110@users.noreply.github.com>
2025-04-22 14:28:36 +0200

    adding astro vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,11 @@ import react from "@astrojs/react";
 
 import mdx from "@astrojs/mdx";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({ mode: "standalone" }),
+  adapter: vercel(),
 
   vite: {
     plugins: [tailwindcss()],
